92507d0f2f34650ac97ed4b5e071dce05123f526
YarCrasy <contact@yarcrasy.com>
2025-11-14 13:33:41 +0000

    backup

--- Web/src/main.jsx
+++ Web/src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { AuthProvider } from './hooks/AuthHooks.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider initialStrategy='firebase'>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
