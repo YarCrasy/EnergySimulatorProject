f91f502039e525e7a06f44f1696a94f780cc3a66
IpialesNatt <natalyipiales@gmail.com>
2025-11-14 01:13:18 +0000

    Footer listo y CardPanel estructurado

--- Web/src/App.jsx
+++ Web/src/App.jsx
@@ -1,32 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Footer from './components/footer/Footer'
 
-// Importing components
-import Header from './components/header/Header'
-
-// Importing page components
-import Home from './pages/home/Home'
-import Simulator from './pages/simulator/Simulator'
-import Projects from './pages/projects/Projects'
-import Contact from './pages/contact/Contact'
-import NotFound from './pages/not-found/NotFound'
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/simulator" element={<Simulator />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+     <div className="app-container" > 
+      <Footer/>
+    </div>
     </>
-  )
+    )
 }
 
 export default App
--- Web/src/components/footer/Footer.jsx
+++ Web/src/components/footer/Footer.jsx
@@ -1,11 +1,31 @@
 import "./Footer.css";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
 function Footer() {
-    return (
-        <footer className="footer-container">
-            <p>&copy; 2024 Simulador de Energía Renovable. Todos los derechos reservados.</p>
-        </footer>
-    );
+  return (
+    <footer className="footer-container">
+      <div className="social-media-container">
+        <a href="https://facebook.com" target="_blank">
+          <FaFacebook size={24} />
+        </a>
+        <a href="https://twitter.com" target="_blank">
+          <FaTwitter size={24} />
+        </a>
+        <a href="https://instagram.com" target="_blank">
+          <FaInstagram size={24} />
+        </a>
+      </div>
+      <div className="footer-contact">
+        <a href="/privacy-contact">Contact</a>
+      </div>
+      <div className="footer-work">
+        <a href="/privacy-work">Work</a>
+      </div>
+      <div className="footer-legals">
+        <a href="/privacy-legals">Legals</a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
