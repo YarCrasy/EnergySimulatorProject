165354e8451edfd55a30b3d75ec07c8cdd414a0c
IpialesNatt <natalyipiales@gmail.com>
2025-11-14 02:51:11 +0000

    Formulario de registro con fondo imagen y estilos, integrado en ruta

--- Web/src/App.jsx
+++ Web/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
 
 // Importing page components
 import Home from './pages/home/Home'
@@ -8,6 +9,7 @@ import Simulator from './pages/simulator/Simulator'
 import Projects from './pages/projects/Projects'
 import Contact from './pages/contact/Contact'
 import NotFound from './pages/not-found/NotFound'
+import Register from './pages/register/Register'
 
 function App() {
 
@@ -20,8 +22,10 @@ function App() {
           <Route path="/simulator" element={<Simulator />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/register" element={<Register />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
     )
