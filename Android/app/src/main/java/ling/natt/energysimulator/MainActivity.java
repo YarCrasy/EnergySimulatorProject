8ef7afe54580199766afc4f9604e5f4d8edeb979
YarCrasy <contact@yarcrasy.com>
2025-12-02 17:29:46 +0000

    Update MainActivity.java

--- Android/app/src/main/java/ling/natt/energysimulator/MainActivity.java
+++ Android/app/src/main/java/ling/natt/energysimulator/MainActivity.java
@@ -19,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
         WebView webView = findViewById(R.id.web);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
