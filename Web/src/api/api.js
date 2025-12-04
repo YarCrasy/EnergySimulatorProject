2d92032c165b88d6a7a85d23f801368c95bbbe0f
YarCrasy <contact@yarcrasy.com>
2025-12-04 11:21:11 +0000

    webDeployCheatsheet

--- Web/src/api/api.js
+++ Web/src/api/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+//scp -P 14784 -r .../Web/dist/* rbp.local:/var/www/web
 const api = axios.create({
   baseURL: "https://dam-project.yarcrasy.com/api",
 });
--- Web/src/api/axios.js
+++ Web/src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+//scp -P 14784 -r .../Web/dist/* rbp.local:/var/www/web
 export default axios.create({
   baseURL: "https://dam-project.yarcrasy.com/api", // << tu backend
   headers: {
